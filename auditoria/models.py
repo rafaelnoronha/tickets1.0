a55aa2c70550f689dc3f6fa9f46cb7368bb56c0b
Rafael Noronha <rafaelsnoronha@hotmail.com>
2021-11-03 13:14:32 -0300

    Criação do modelo de Auditoria

--- auditoria/models.py
+++ auditoria/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from usuarios.models import Usuario
+import uuid
 
-# Create your models here.
+
+TIPO_OPERACAO_CHOISES = [
+    ('CREATE', 'CREATE'),
+    ('UPDATE', 'UPDATE'),
+    ('DELETE', 'DELETE')
+]
+
+
+class Auditoria(models.Model):
+    uuid = models.UUIDField(
+        verbose_name='UUID',
+        default=uuid.uuid4(),
+        help_text='UUID Código único não sequencial',
+    )
+
+    data_operacao = models.DateField(
+        verbose_name='Data da Operação',
+        auto_now_add=True,
+        help_text='Data da execução da operação',
+    )
+
+    hora_operacao = models.TimeField(
+        verbose_name='Hora da Operação',
+        auto_now_add=True,
+        help_text='Hora da execução da operação',
+    )
+
+    tipo_operacao = models.CharField(
+        verbose_name='Tipo de Operação',
+        choices=TIPO_OPERACAO_CHOISES,
+        help_text='Tipo de operação realizada',
+    )
+
+    usuario_operacao = models.ForeignKey(
+        Usuario,
+        verbose_name='Usuário da Operação',
+        on_delete=models.PROTECT,
+        related_name='usuario',
+        help_text='Usuário responsável pela operação realizada',
+    )
+
+    estado_anterior = models.TextField(
+        verbose_name='Estado Anterior',
+        default='',
+        help_text='Estado anterior do registro afetado',
+    )
+
+    estado_atual = models.TextField(
+        verbose_name='Estado Atual',
+        default='',
+        help_text='Estado atual do registro afetado',
+    )
+
+    class Meta:
+        ordering = ['-id']
+        db_table = 'auditoria'
+        verbose_name = 'Auditoria'
+        verbose_name_plural = 'Auditoria'
+
+    def __str__(self):
+        return f'{self.id} - {self.tipo_operacao} [{self.usuario_operacao}]'
