972d44814675daccd72b4ed2a34bf3962cfe1761
Rafael Noronha <rafaelsnoronha@hotmail.com>
2022-03-14 17:48:49 -0300

    Inicio das permissões

--- empresa/views.py
+++ empresa/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Empresa
 from .serializer import EmpresaSerializer
 from .filters import EmpresaFilter
+from .permissions import EmrpesaPemission
 
 
 class EmpresaViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,4 @@ class EmpresaViewSet(viewsets.ModelViewSet):
     lookup_field = 'uuid'
     serializer_class = EmpresaSerializer
     filterset_class = EmpresaFilter
+    permission_classes = (EmrpesaPemission, )
