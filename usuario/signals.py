5864bf61881919c71644805b05465c44e0d5b6b2
Rafael Noronha <rafaelsnoronha@hotmail.com>
2022-03-29 17:45:49 -0300

    Início da implementação dos logs no app de usuario

--- usuario/signals.py
+++ usuario/signals.py
@@ -1,15 +1,10 @@
 from .models import Usuario
 from auditoria.models import Auditoria
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save
+from django.core.signals import request_finished
 
 
 @receiver(pre_save, sender=Usuario)
 def antes_de_salvar_usuario(sender, instance, **kwargs):
     instance.set_password(instance.password)
-
-
-@receiver(post_save, sender=Usuario)
-def depois_de_salvar_usuario(sender, instance, **kwargs):
-    #
-    pass
--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, mixins
 from .models import Usuario, LogAutenticacao
+from auditoria.models import Auditoria
 from .filters import UsuarioFilter, LogAutenticacaoFilter
 from core.permissions import BasePemission
 from django.contrib.auth.models import Group, Permission
@@ -7,6 +8,8 @@ from .serializer import UsuarioSerializer, UsuarioSerializerCreate, UsuarioSeria
                         UsuarioSerializerUpdatePartialUpdate, LogAutenticacaoSerializer, \
                         LogAutenticacaoSerializerRetrieve, GrupoPermissoesUsuarioSerializer, \
                         PermissaoUsuarioSerializer, GrupoPermissoesUsuarioSerializerCreateUpdatePartialUpadate
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
@@ -24,6 +27,35 @@ class UsuarioViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, UsuarioSerializer)
+
+    def update(self, request, *args, **kwargs):
+        usuario_antes_da_alteracao = Usuario.objects.get(id=self.get_object().id)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        Auditoria.objects.create(tabela_operacao='usuario', tipo_operacao='UPDATE', usuario_operacao=request.user,
+                                 estado_anterior=usuario_antes_da_alteracao, estado_atual=self.get_object())
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        Auditoria.objects.create(tabela_operacao='usuario', tipo_operacao='CREATE', usuario_operacao=request.user,
+                                 estado_anterior='', estado_atual=self.get_object())
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class LogAutenticacaoViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin,
